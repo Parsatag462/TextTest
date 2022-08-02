1dd0d8ab864f1f2c48cd4abdd315249fe84ed225
Parsatag462 <mr.nitsuji462@gmail.com>
2022-08-02 14:57:09 +0000

    texttest

---
+++ text-test/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TextTest from './Components/TextTest';
+
+function App() {
+  return (
+    <div className="App">
+      <TextTest/>
+    </div>
+  );
+}
+
+export default App;
